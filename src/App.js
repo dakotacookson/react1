11660822672029a8afb1d036d7db39a156562ebc
unknown <dakota.cookson1995@gmail.com>
2019-02-28 10:48:22 -0600

    m

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import logo from './fire-border-transparent-yin-yang.png';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      
+      <div className="App">
+        <header className="App-header">
+<div class="square12"></div>
+<div class="square11"></div>
+<div class="square10"></div>
+<div class="square9"></div>
+<div class="square8"></div>
+<div class="square7"></div>
+<div class="square6"></div>
+<div class="square5"></div>
+<div class="square4"></div>
+<div class="square3"></div>
+<div class="square2"></div>
+<div class="square"></div>
+<div class="square0"></div>
+
+
+
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
